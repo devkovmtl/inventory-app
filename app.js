d068c0a893b51634b5b06f62c72bf2d214fefaa8
Mikael <dev.kov.mtl@gmail.com>
2022-03-14 16:34:27 -0400

    Setup nodemon and added script and Procfile

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+
+const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOST, MONGO_DATABASE } =
+  process.env;
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
